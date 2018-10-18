55070cb7647e9aa6f19433e300b160181ce9fa07
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-18 10:06:21 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # py
-### kb
+### kgdb
 
+(c) Dmitry Ponyatov <<dponyatov@gmail.com>> , All rights reserved
+
+github: https://github.com/ponyatov/py
+
+manual: https://ponyatov.github.io/py/
--- py.py
+++ py.py
@@ -1,15 +1,136 @@
 ## @file
+## @brief in-memory knowledge/graph/object database
 
 import os,sys,time
 
-try:                SRCFILE = sys.argv[1]
-except IndexError:  SRCFILE = 'src.src'
+## @ingroup interp
+## @{
+
+try:
+    ## source code file
+    SRCFILE = sys.argv[1]
+except IndexError:
+    SRCFILE = 'src.src'
 
 ## init file will be parser on start
 SRC = open(SRCFILE).read()
 
-try:                DBFILE = sys.argv[2]
-except IndexError:  DBFILE = sys.argv[0]+'.db'
+## @}
+
+## @ingroup db
+## @{
+
+try:
+    ## persistent database file (vocabulary snapshot)
+    DBFILE = sys.argv[2]
+except IndexError:
+    DBFILE = sys.argv[0]+'.db'
+    
+## @}
+
+## @defgroup sym Symbolic class system
+## @dot
+## digraph {
+## rankdir=LR;
+## Object -> Primitive;
+## Object -> Container;
+## Object -> Active;
+## Object -> Doc;
+## }
+## @enddot
+## @{
+
+## base generic object
+class Object:
+    
+    ## constructor
+    def __init__(self,V):
+        ## class/type tag
+        self.type = self.__class__.__name__.lower()
+        ## single primitive value
+        self.value = V
+
+    ## @defgroup print print/dump
+    ## @brief represent any object in text forms
+    ## @{
+    
+    ## represent any object in text form
+    def __repr__(self):
+        return self.dump()
+    
+    ## full dump of any object in tree form 
+    def dump(self,depth=0):
+        return self.head()
+    
+    ## short dump of any object: header only
+    def head(self,prefix=''):
+        return '%s<%s:%s>'%(prefix,self.type,self.value)
+    
+    ## @}
+
+## @defgroup prim primitives
+## @brief close to machine-level or VM implementation language  
+## @{
+
+## machine-level primitives
+class Primitive(Object): pass
+
+## @}
+
+## @defgroup cont containers
+## @brief data elements grouping
+## @{
+
+## data container
+class Container(Object): pass
+
+## ordered vector
+class Vector(Container): pass
+
+## LIFO stack
+class Stack(Container): pass
+
+## associative array
+class Map(Container): pass
+
+## @}
+
+## @defgroup active active objects
+## @brief has execution semantics
+## @{
+
+## callable objects has execution semantics
+class Active(Object): pass
+
+## @}
+
+## @defgroup doc documenting
+## @{
+
+## documenting
+class Doc(Object): pass
+
+## @}
+
+## @}
+
+## @defgroup fvm FORTH Virtual Machine
+## @{
+
+## @defgroup stack stack
+## @{
+
+## data stack
+S = Stack('DATA')
+
+## @}
+
+## @defgroup voc vocabulary
+## @}
+
+## @}
+
+## @}
 
 ## @defgroup parser Syntax parser
 ## @{
@@ -17,25 +138,38 @@ except IndexError:  DBFILE = sys.argv[0]+'.db'
 import ply.lex  as lex
 import ply.yacc as yacc
 
+## token list will be parsed
 tokens = ['SYM']
 
+## ignore spaces
+t_ignore = ' \t\r'
+
+## ignore comments
 t_ignore_COMMENT = '\#.*'
 
+## increment line counter
 def t_newline(t):
     r'\n'
     t.lexer.lineno += 1
 
+## symbol
 def t_SYM(t):
     r'[a-zA-Z0-9_]+'
     return t
 
+## lexer error callback
 def t_error(t): raise SyntaxError(t)
 
+## syntax lexer
 lexer = lex.lex()
-lexer.input(SRC)
 
 ## @}
 
+## @defgroup interp interpreter
+## @ingroup fvm
+## @{
+
+## interpret given string
 def INTERPRET(SRC):
     lexer.input(SRC)
     while True:
@@ -44,3 +178,20 @@ def INTERPRET(SRC):
         print '<%s>' % token
 
 INTERPRET(SRC)
+
+print S
+
+## @}
+
+## @defgroup web Web interface
+## @{
+
+import socket
+
+## tcp/ip address to bind
+IP   = '127.0.0.1'
+
+## tcp/ip port to bind
+PORT = 8888
+
+## @}
